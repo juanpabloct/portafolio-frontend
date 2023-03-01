17431d8cf2c59ea50c3304e740bbf0708a074d54
juanpabloct <juanptorres2003@gmail.com>
2023-02-28 21:14:09 -0500

    Se creo componente llamado Notification

--- src/components/Utilities/notification.tsx
+++ src/components/Utilities/notification.tsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { Check } from "../../assets/icons/inputPassword/CheckAndError/check";
 import { useSession } from "../../customHooks/customHookSession";
 import { useEffect } from "react";
-import { ChangeCorrect } from "../../reducers/Reducer.sessions";
+import { ChangeCorrectShow } from "../../reducers/Reducer.sessions";
 import { ErrorIcons } from "../../assets/icons/inputPassword/CheckAndError/errorIcons";
 export const Notification = () => {
   const { correct } = useSession();
@@ -10,7 +10,7 @@ export const Notification = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const timeChangeShow = setTimeout(() => {
-      dispatch(ChangeCorrect({ ...correct, show: false }));
+      dispatch(ChangeCorrectShow());
     }, 4000);
     return () => {
       clearTimeout(timeChangeShow);
@@ -23,7 +23,7 @@ export const Notification = () => {
         isCorrect ? "border-slate-400" : "border-red-500 text-red-500"
       } ${
         show ? "visible opacity-100" : "invisible opacity-0"
-      } transition-["visible"] duration-1000 delay-200 animate-notificationOpacity `}
+      } transition-["visible"] duration-500  animate-notificationOpacity `}
     >
       {isCorrect ? <Check size="24" /> : <ErrorIcons size="24" />}
       <p className="text-lg">{message}</p>
