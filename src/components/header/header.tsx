14a6109831a298ee6a32e4650a3ee87c0de2946c
juanpabloct <juanptorres2003@gmail.com>
2023-03-10 01:44:05 -0500

    Se le agrego una altura al head

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="w-full bg-white border-b-2 border-[#5c6d93]  text-[#5c6d93] text-center text-2xl font-bold py-2">
+    <header className="w-full bg-white border-b-2 border-[#5c6d93]  text-[#5c6d93] text-center text-2xl font-bold py-2 h-[10%] flex items-center justify-center">
       <h1>My Portafolio</h1>
     </header>
   );
