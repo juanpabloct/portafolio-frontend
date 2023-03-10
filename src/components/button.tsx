a0929c66fe93b71377238e25e0bdd32755e5ef3b
juanpabloct <juanptorres2003@gmail.com>
2023-03-10 01:36:56 -0500

    se agrego padding al botton

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,15 @@
 interface PropsButton {
   action: () => void;
   text: string;
-  validate: boolean;
+  validate?: boolean;
 }
 export const Button = ({ action, text, validate }: PropsButton) => {
   return (
     <button
       type="submit"
-      className={`border-sky-900 border rounded-md w-1/2 ${
+      className={`border-sky-900 border rounded-md px-8 py-[0.2rem] ${
         validate ? "bg-neutral-500 " : "bg-[#5c6d93] cursor-pointer"
-      } font-medium text-lg text-white py-1 `}
+      } font-medium text-base text-white  `}
       disabled={validate}
       onSubmit={(e) => e.preventDefault()}
       onClick={() => {
