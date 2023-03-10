2359aca6cd26e89214766b6e3453c0fc1c9fcef3
juanpabloct <juanptorres2003@gmail.com>
2023-03-10 01:41:21 -0500

    Modificacion de estilos

--- src/pages/accesUser.tsx
+++ src/pages/accesUser.tsx
@@ -4,30 +4,28 @@ import { justificar, Tabs, types } from "../components/Utilities/tabs";
 export const daata = "";
 export const AccesUser = () => {
   return (
-    <div className="h-full">
-      <Tabs
-        width={"50%"}
-        type={types.without_borders}
-        justify={justificar.center}
-        tabs={[
-          {
-            element: (
-              <section className="App justify-center  flex  w-1/2  max-w-[624px]  h-auto shadow-md shadow-zinc-500 border border-zinc-300 rounded-md items-center bg-white">
-                <Login />
-              </section>
-            ),
-            name: "Login",
-          },
-          {
-            element: (
-              <section className="App justify-center   flex  w-1/2  max-w-[624px]  h-auto shadow-md shadow-zinc-500 border border-zinc-300 rounded-md items-center bg-white">
-                <CreateUser />
-              </section>
-            ),
-            name: "Crear Usuario",
-          },
-        ]}
-      />
-    </div>
+    <Tabs
+      width={"50%"}
+      type={types.without_borders}
+      justify={justificar.center}
+      tabs={[
+        {
+          element: (
+            <section className="App justify-center  flex  w-1/2  max-w-[624px]  h-auto shadow-md shadow-zinc-500 border border-zinc-300 rounded-md items-center bg-white">
+              <Login />
+            </section>
+          ),
+          name: "Login",
+        },
+        {
+          element: (
+            <section className="App justify-center   flex  w-1/2  max-w-[624px]  h-auto shadow-md shadow-zinc-500 border border-zinc-300 rounded-md items-center bg-white">
+              <CreateUser />
+            </section>
+          ),
+          name: "Crear Usuario",
+        },
+      ]}
+    />
   );
 };
