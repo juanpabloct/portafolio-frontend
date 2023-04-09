eb5be15ec484f6385f242fba42ca3cf41802885a
juanpabloct <juanptorres2003@gmail.com>
2023-04-09 14:12:17 -0500

    Style with tailwind header

---
+++ src/components/styles/headerNav.tsx
@@ -0,0 +1,4 @@
+import tw from "tailwind-styled-components";
+
+export const HeaderNav = tw.header`w-full bg-white text-[#5c6d93] text-center text-2xl font-bold h-[10%]   flex flex-col  justify-center  border-b-2 border-black
+`;
